10d54a565bc39306f8e3508cea926320a09cfe2a
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-14 14:19:47 -0500

    initial deck class created and commited

---
+++ deck.rb
@@ -0,0 +1,22 @@
+require_relative 'flashcard.rb'
+
+class Deck
+  def initialize(answer_def_hash)
+    @deck_of_cards = []
+    create_flash_cards(answer_def_hash)
+  end
+
+  def create_flash_cards(answer_def_hash)
+    answer_def_hash.each do |pair|
+      @deck_of_cards << Flashcard.new(pair)
+    end
+  end
+
+  def is_correct?(i, input)
+    @deck_of_cards[i].is_correct?(input)
+  end
+
+  def get_definition(i)
+    @deck_of_cards[i].definition
+  end
+end
