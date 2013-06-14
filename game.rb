b0581b9ff74d8a43a9a1e514b8d5d27f7f88e9af
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-14 14:51:25 -0500

    Troubleshot method calls with deck

--- game.rb
+++ game.rb
@@ -8,11 +8,12 @@ class Game
   def create_deck(filename)
     flashcard_hashes = []
     text_parser(filename).each do |line|
-    flashcard_hashes << {:definition => line[0], :answer => line[1]}
-    @deck = Deck.new(flashcard_hashes)
+      flashcard_hashes << {:definition => line[0], :answer => line[1]}
+      @deck = Deck.new(flashcard_hashes)
+    end
   end
 
-  def start
+  def play
     @deck.size.times do |index|
       puts @deck.get_definition(index)
       until @deck.is_correct?(index, input)
